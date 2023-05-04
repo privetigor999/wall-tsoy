8e9bffec7dd2f680e7ea5803db7056d0803175fc
Igor Ershov <privetie@yandex.ru>
2023-05-04 16:38:07 +0300

    localStorage

--- src/components/Header/HeaderMobileNavigationList/ChangerLanguageMobile/ChangerLanguageMobile.tsx
+++ src/components/Header/HeaderMobileNavigationList/ChangerLanguageMobile/ChangerLanguageMobile.tsx
@@ -1,27 +1,21 @@
 import React from "react";
 import i18next from "i18next";
 import { languages } from "../../../../utils/data/languages";
-import { useAppDispatch, useAppSelector } from "../../../../hooks/redux-hooks";
-import { setCurrentLanguage } from "../../../../store/languageReducer/languageReducer";
 import { useTranslation } from "react-i18next";
 
 import "./changerLanguageMobile.scss";
 
 export const ChangerLanguageMobile: React.FC = () => {
-  const dispatch = useAppDispatch();
   const { t } = useTranslation();
-
-  const currentLanguage = useAppSelector(
-    (state) => state.language.currentLanguage
-  );
+  const storedLanguage = localStorage.getItem("language") || "ru";
 
   const handleChangeLang = (lang: string): void => {
-    dispatch(setCurrentLanguage(lang));
+    localStorage.setItem("language", lang);
     i18next.changeLanguage(lang);
   };
 
   const classNameButton = (lang: string): string => {
-    return `changerLanguageMobile__button ${lang === currentLanguage &&
+    return `changerLanguageMobile__button ${lang === storedLanguage &&
       "changerLanguageMobile__button--active"}`;
   };
 
